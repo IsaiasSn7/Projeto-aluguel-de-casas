34b24fbca6d2143ad51ee08d0d6d880af2ddc4b5
isaiassn7 <isaiassousa19092000@gmail.com>
2025-07-11 08:51:13 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-      <Inicio />
-      {/* <Entrar /> */}
+      {/* <Inicio /> */}
+      <Entrar />
     </>
   )
 }
--- src/pages/Entrar.jsx
+++ src/pages/Entrar.jsx
@@ -1,38 +1,56 @@
+import { useState } from "react";
 import logoBranca from "../img/logo-branca.png";
 
 const Entrar = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <>
-      <div className="bg-Roxo flex h-screen w-screen items-center justify-center">
-        <div className="bg-white p-[10px]">
-            <box-icon name="left-arrow-alt" color="#2b0464"></box-icon>
-          <div className="flex h-[400px] w-[400px] flex-col items-center justify-center">
-            <img className="mb-[30px]" src={logoBranca} alt="logo-branca" />
-            <form className="flex flex-col text-[16px]">
-              <label className="mb-[6px]">Email</label>
+    <div className="bg-Roxo flex h-screen w-screen items-center justify-center">
+      <div className="bg-white p-[10px] w-[350px] rounded-[5px] lg:w-[400px]">
+        <box-icon name="left-arrow-alt" color="#2b0464"></box-icon>
+        <div className="flex h-[400px] flex-col items-center justify-center">
+          <img className="mb-[40px]" src={logoBranca} alt="logo-branca" />
+          <form className="flex flex-col text-[16px] mb-[20px]">
+            <label className="mb-[6px]">Email</label>
+            <input
+              className="mb-[20px] h-[45px] w-[300px] rounded-[5px] bg-gray-200 border border-gray-300 focus:border-Laranja outline-none px-4 py-2 duration-200"
+              type="email"
+            />
+
+            <label className="mb-[6px]">Senha</label>
+            <div className="relative w-[300px]">
               <input
-                className="mb-[15px] h-[45px] w-[300px] rounded-[5px] bg-gray-200"
-                type="email"
+                className="h-[45px] w-full rounded-[5px] bg-gray-200 border border-gray-300 focus:border-Laranja outline-none px-4 py-2 pr-10 duration-200"
+                type={showPassword ? "text" : "password"}
               />
-              <label className="mb-[6px]">Senha</label>
-              <input
-                className="h-[45px] w-[300px] rounded-[5px] bg-gray-200"
-                type="password"
-              />
-            </form>
-            <div>
-              <a className="text-[12px]" href="#">
-                Esqueci minha senha
-              </a>
-              <a className="text-[12px]" href="#">
-                Criar conta
-              </a>
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 mt-[8.5%] -translate-y-1/2 text-gray-700"
+              >
+                <box-icon
+                  name={showPassword ? "hide" : "show"}
+                  color="#4b5563" // Tailwind text-gray-700
+                ></box-icon>
+              </button>
             </div>
-            <button>Entrar</button>
+          </form>
+
+          <div className="mb-[20px] flex justify-between w-[230px]">
+            <a className="text-[13px] block hover:text-Laranja duration-200" href="#">
+              Esqueci minha senha
+            </a>
+            <a className="text-[13px] block hover:text-Laranja duration-200" href="#">
+              Criar conta
+            </a>
           </div>
+
+          <button className="w-[300px] bg-Roxo h-[45px] rounded-[5px] text-Laranja hover:scale-105 duration-300">
+            Entrar
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
